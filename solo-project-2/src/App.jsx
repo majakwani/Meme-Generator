0d3f65adc946da07b7029cc1e41ec96e90b7f377
majakwani <anasjakwani72@gmail.com>
2022-09-01 12:38:37 +0500

    Changed props input type from manual inputs to input from an object

--- solo-project-2/src/App.jsx
+++ solo-project-2/src/App.jsx
@@ -1,13 +1,23 @@
 import { Header } from "../components/Header"
 import { Hero } from "../components/Hero"
 import { Card } from "../components/Card"
+import data from '../components/data';
+
+const cardData = data.map((card) => {
+  return <Card 
+  data = {card}
+  star = "../src/Star.png"
+  />
+})
 
 function App() {
  return (
   <div className="container">
     <Header />
     <Hero />
-    <Card />
+    <section className="cardContainer">
+    {cardData}
+    </section>
   </div>
  )
 }
