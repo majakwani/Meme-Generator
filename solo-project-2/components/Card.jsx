07ec73e588e5ac70eb49914b9f7741daf487a93c
majakwani <anasjakwani72@gmail.com>
2022-09-01 12:38:06 +0500

    Changed props input type from manual inputs to input from an object

--- solo-project-2/components/Card.jsx
+++ solo-project-2/components/Card.jsx
@@ -1,17 +1,24 @@
 import React from "react";
-import image from '../src/image12.png'
-import star from '../src/Star.png'
 
-export function Card(){
+export function Card(props){
+    let cardText;
+    if(props.data.openSpots === 0){
+        cardText = "SOLD OUT"
+    }
+    else if (props.data.location === "Online"){
+        cardText = "ONLINE"
+    }
+
     return (
         <div className="card">
-            <img src= {image} className = "cardImage" />
-            <p className="firstPara"><img src = {star} className = "star" /> <span className="span-one">5.0</span> (6) • USA</p>
+            {cardText && <div className="cardText">{cardText}</div>}
+            <img src= {`../src/${props.data.coverImg}`} className = "cardImage" />
+            <p className="firstPara"><img src = {props.star} className = "star" /> <span className="span-one">{props.data.stats.rating}</span> ({props.data.stats.reviewCount}) • {props.data.location}</p>
             <p className="secondPara">
-                Life lessons with Katie Zaferes
+                {props.data.title}
             </p>
             <p className="thirdPara">
-                <span className="bold">From $136</span> / person
+                <span className="bold">{props.data.price}</span> /person
             </p>
         </div>
     )
